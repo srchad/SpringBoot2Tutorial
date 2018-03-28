b3b0efbf96f33ddb443b4db80e634551b7254960
kai.1001 <kai.1001@kakaocorp.com>
2018-03-28 18:39:47 +0900

    프로젝트 생성

---
+++ src/main/java/com/tistory/lky1001/boot2tutorial/Boot2tutorialApplication.java
@@ -0,0 +1,12 @@
+package com.tistory.lky1001.boot2tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2tutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot2tutorialApplication.class, args);
+	}
+}
