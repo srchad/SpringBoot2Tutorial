86d2c8e36e44b6d6e185b4023a0ed912cbbf31db
frank.park <frank.park@kakaocorp.com>
2018-04-03 22:43:30 +0900

    testing hotswap & auto reloading

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,10 +14,11 @@ public class DemoApplication {
 	}
 
 	@RestController
+    @RequestMapping("api")
     public static class MainController{
-	    @GetMapping("")
-        public String index(){
-	        return "Hello World!!";
+	    @GetMapping
+        public String helloWorld(){
+	        return "Hello World!";
         }
     }
 }
