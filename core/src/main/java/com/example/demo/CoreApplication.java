83ef486c879efae28936da639ed8cea2c594e189
frank.park <frank.park@kakaocorp.com>
2018-04-04 00:14:11 +0900

    23.4 Fluent Builder API  - multimodule , sources, child

---
+++ core/src/main/java/com/example/demo/CoreApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CoreApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+	}
+}
--- web/src/main/java/com/example/demo/WebApplication.java
+++ web/src/main/java/com/example/demo/WebApplication.java
@@ -1,19 +1,21 @@
 package com.example.demo;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WebApplication {
 
 	public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(DemoApplication.class);
-        app.setBannerMode(Banner.Mode.LOG);
-        app.run(args);
+        new SpringApplicationBuilder()
+                .sources(CoreApplication.class)
+                .child(WebApplication.class)
+                .bannerMode(Banner.Mode.LOG)
+                .run(args);
 	}
 
 	@RestController
