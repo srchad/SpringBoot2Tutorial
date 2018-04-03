aaf0b8683ef473b4bddadfcce809a790fe439518
frank.park <frank.park@kakaocorp.com>
2018-04-03 22:32:14 +0900

    20. Developer Tools

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +11,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@RestController
+    public static class MainController{
+	    @GetMapping("")
+        public String index(){
+	        return "Hello World!!";
+        }
+    }
 }
