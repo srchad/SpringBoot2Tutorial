6b1dc7b1b15447453df5e049d3c97cb2f5b7b1f0
genos <genos.lee@kakaocorp.com>
2018-04-03 23:12:54 +0900

    banner mode on with png and text file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class DemoApplication {
 
     public static void main(String[] args) {
-//        SpringApplication app = new SpringApplication(DemoApplication.class);
-//        app.setBannerMode(Banner.Mode.OFF);
-//        app.run(args);
-
-        new SpringApplicationBuilder()
-                .sources(DemoApplication.class)
-                .bannerMode(Banner.Mode.OFF)
-                .run(args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
