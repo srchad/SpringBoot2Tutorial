4c88fdbcd80f103775efaa9a6cdecbe9aa54c56d
frank.park <frank.park@kakaocorp.com>
2018-04-04 00:29:36 +0900

    23.5 Application Events and Listeners

--- core/src/main/java/com/example/demo/CoreApplication.java
+++ core/src/main/java/com/example/demo/CoreApplication.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 
+@Slf4j
 @SpringBootApplication
 public class CoreApplication {
-
 	public static void main(String[] args) {
         SpringApplication app = new SpringApplication(CoreApplication.class);
         app.setBannerMode(Banner.Mode.OFF);
--- web/src/main/java/com/example/demo/WebApplication.java
+++ web/src/main/java/com/example/demo/WebApplication.java
@@ -1,20 +1,32 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) {
+
         new SpringApplicationBuilder()
                 .sources(CoreApplication.class)
                 .child(WebApplication.class)
                 .bannerMode(Banner.Mode.LOG)
+                .listeners((ApplicationListener<ApplicationStartingEvent>) event -> log.info("Application is starting..."))
+                .listeners((ApplicationListener<ApplicationEnvironmentPreparedEvent>) event -> log.info("Application environment is prepared."))
+                .listeners((ApplicationListener<ApplicationStartedEvent>) event -> log.info("Application is started at {}.", event.getTimestamp()))
+                .listeners((ApplicationListener<ApplicationReadyEvent>) event -> log.info("Application is ready now."))
                 .run(args);
 	}
 
