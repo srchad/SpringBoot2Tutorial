f335709039a23d053e6aaf38215af02e1f1177dc
frank.park <frank.park@kakaocorp.com>
2018-04-04 10:33:06 +0900

    23.6 Web Environment

--- core/src/main/java/com/example/demo/CoreApplication.java
+++ core/src/main/java/com/example/demo/CoreApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Slf4j
 @SpringBootApplication
 public class CoreApplication {
 	public static void main(String[] args) {
         SpringApplication app = new SpringApplication(CoreApplication.class);
+        app.setApplicationContextClass(AnnotationConfigApplicationContext.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
 	}
