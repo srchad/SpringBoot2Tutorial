49d6841e1aba164f35e20dd46907c3d29d25b694
genos <genos.lee@kakaocorp.com>
2018-04-04 10:19:49 +0900

    set spring profile default and dev using value property in HelloController

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ refererence: https://docs.spring.io/spring-boot/docs/2.0.0.RELEASE/reference/htm
 
 jar file 생성: ./gradlew build
 build 과정 디버그: java -jar myproject-0.0.1-SNAPSHOT.jar --debug
+
+/next: 다음 발표자 지목 api
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author genos.lee <genos.lee@kakaocorp.com>
  */
 @RestController
 public class HelloController {
+
+    @Value("${hello.text}")
+    private String text;
+
+    @Value("${hello.number.in.range}")
+    private int randomNumber;
+
+    @Value("${hello.members}")
+    private String[] members;
+
     @GetMapping("/")
     public String test(){
-        return "test";
+        return text;
     }
 
+    @GetMapping("/next")
+    public String next() {
+        return members[randomNumber];
+    }
 }
