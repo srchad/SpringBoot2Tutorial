5d1dc53e84470bb623adce085a35b87540e1dda0
genos <genos.lee@kakaocorp.com>
2018-04-10 23:31:17 +0900

    mustache template 연동

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,10 +3,14 @@ package com.example.demo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author genos.lee <genos.lee@kakaocorp.com>
@@ -31,7 +35,10 @@ public class HelloController {
     }
 
     @GetMapping("/next")
-    public String next() {
-        return members[randomNumber];
+    public ModelAndView next()
+    {
+        ModelAndView result = new ModelAndView();
+        result.addObject("id",  members[randomNumber]);
+        return result;
     }
 }
