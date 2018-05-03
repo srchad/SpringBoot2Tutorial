158a8f1705432cc28a9ba2d6e483178b978ba54e
elon.kwon <elon.kwon@kakaocorp.com>
2018-05-03 10:04:34 +0900

    부트스트랩 pricing 페이지 적용.

---
+++ part5/gradle-example/src/main/java/com/example/gradleexample/controller/MyController.java
@@ -0,0 +1,19 @@
+package com.example.gradleexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/pricing")
+    public String pricing() {
+        return "pricing";
+    }
+
+}
