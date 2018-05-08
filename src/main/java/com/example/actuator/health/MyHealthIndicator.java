35e3c3a2b70970c13793564f31d718176982278f
daniel.94 <daniel.94@kakaocorp.com>
2018-05-08 16:53:53 +0900

    actuator

---
+++ src/main/java/com/example/actuator/health/MyHealthIndicator.java
@@ -0,0 +1,14 @@
+package com.example.actuator.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        return Health.up().build();
+    }
+}
